d9f487b05492b21d5ca76515a2ce73454bd0156f
Patrick Gould <patrick.gould94@gmail.com>
2020-12-24 01:21:13 -0500

    Added navigation bar, side panel, and footer

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <!-- <v-app>
     <v-app-bar
       app
       color="primary"
@@ -40,21 +40,67 @@
     <v-main>
       <HelloWorld/>
     </v-main>
-  </v-app>
+  </v-app> -->
+
+  <v-card class="mx-auto overflow-hidden">
+    <v-navigation-drawer v-model="drawer" absolute temporary right app>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      absolute
+      color="white"
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
+      app
+    >
+      <v-spacer></v-spacer>
+
+      <v-toolbar-title>Subaru Crosstrek</v-toolbar-title>
+
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-main> </v-main>
+
+    <v-footer padless app>
+      <v-col class="text-center" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Simps International</strong>
+      </v-col>
+    </v-footer>
+  </v-card>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 
   data: () => ({
-    //
+    drawer: false,
+    group: null,
   }),
 };
 </script>
